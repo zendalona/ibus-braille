66877b3fa93fc2b89af38912d23c28fed1596216
Nalin.x.Linux <Nalin.x.Linux@gmail.com>
2014-05-29 21:32:32 +0530

    Dispatched from garage after one month

--- engine/engine.py
+++ engine/engine.py
@@ -1,8 +1,8 @@
 # vim:set et sts=4 sw=4:
 #
-# ibus-sharada-braille - The braille ibus engine
+# ibus-sharada-braille - The Braille Input Bus project
 #
-# Copyright (c) 2007-2011 Peng Huang <shawn.p.huang@gmail.com>
+# Copyright (c) 20014-2015 Nalin.x.Linux <Nalin.x.Linux@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,145 +20,205 @@
 
 import gobject
 import pango
-import enchant
+import os
 import ibus
 from ibus import keysyms
 from ibus import modifier
+from espeak import espeak
+
+#Where the data is located
+data_dir = "/usr/share/ibus-sharada-braille";
 
 class Engine(ibus.EngineBase):
-    __dict = enchant.Dict("en")
-
     def __init__(self, bus, object_path):
         super(Engine, self).__init__(bus, object_path)
+        self.pressed_keys = u""
+        
+        #Key code map
+        self.keycode_map = {30:"a",31:"s",32:"d",33:"f",34:"g",35:"h",36:"j",37:"k",38:"l",39:";"}
+        
+        #Braille Iter's
+        self.braille_letter_map_pos = 0;
+        
+        #capital switch
+        self.capital_switch = 0;
+
         self.__is_invalidate = False
         self.__preedit_string = u""
         self.__lookup_table = ibus.LookupTable()
         self.__prop_list = ibus.PropList()
-        self.__prop_list.append(ibus.Property(u"test", icon = u"ibus-locale"))
+        self.__prop_list.append(ibus.Property(u"test", icon = u"ibus-locale"))		
 
+        #Load the first language by default
+        self.load_map("malayalam ml")
+    
+    def do_enable (self):
+        # Tell the input-context that the engine will utilize
+        # surrounding-text:
+        self.get_surrounding_text()
+        self.do_focus_in()        
+        
     def process_key_event(self, keyval, keycode, state):
-        # ignore key release events
-        is_press = ((state & modifier.RELEASE_MASK) == 0)
-        if not is_press:
-            return False
+		is_press = ((state & modifier.RELEASE_MASK) == 0)
+		
+		#Key Release
+		if not is_press:
+			ordered_pressed_keys = self.order_pressed_keys(self.pressed_keys);
+			self.pressed_keys = "";
+			
+			#Move map position to contraction if any
+			if ordered_pressed_keys in self.contractions_dict.keys():
+				self.braille_letter_map_pos = self.contractions_dict[ordered_pressed_keys];
+			
+			#Toggle Punctuation
+			elif ordered_pressed_keys == ";":
+				self.braille_letter_map_pos = 2;
+			
+			#Expand Abbreviation
+			elif ordered_pressed_keys == "a":
+				#self.pressed_keys = "";
+				surrounding_text = self.get_surrounding_text()
+				text = surrounding_text[0].get_text()#.decode('UTF-8')
+				cursor_pos = surrounding_text[1]
+				string_up_to_cursor = text[:cursor_pos];
+				last_word = string_up_to_cursor.split()[-1]
+				
+								
+				#Substitute abbreviation if exist and letter bofore the cursor is not space
+				if (last_word in self.abbreviations.keys() and string_up_to_cursor[-1] != " "):
+					self.delete_surrounding_text(-(len(last_word)),len(last_word));
+					for key,value in self.abbreviations.iteritems():
+						if key == last_word:
+							self.__commit_string(value)
+					#Fixme Why this heck is not working :( ??
+					#self.__commit_string(self.abbreviations[last_word.decode('UTF-8')].decode('UTF-8'))
 
-        if self.__preedit_string:
-            if keyval == keysyms.Return:
-                self.__commit_string(self.__preedit_string)
-                return True
-            elif keyval == keysyms.Escape:
-                self.__preedit_string = u""
-                self.__update()
-                return True
-            elif keyval == keysyms.BackSpace:
-                self.__preedit_string = self.__preedit_string[:-1]
-                self.__invalidate()
-                return True
-            elif keyval == keysyms.space:
-                if self.__lookup_table.get_number_of_candidates() > 0:
-                    self.__commit_string(self.__lookup_table.get_current_candidate().text)
-                else:
-                    self.__commit_string(self.__preedit_string)
-                return False
-            elif keyval >= keysyms._1 and keyval <= keysyms._9:
-                index = keyval - keysyms._1
-                candidates = self.__lookup_table.get_canidates_in_current_page()
-                if index >= len(candidates):
-                    return False
-                candidate = candidates[index].text
-                self.__commit_string(candidate)
-                return True
-            elif keyval == keysyms.Page_Up or keyval == keysyms.KP_Page_Up:
-                self.page_up()
-                return True
-            elif keyval == keysyms.Page_Down or keyval == keysyms.KP_Page_Down:
-                self.page_down()
-                return True
-            elif keyval == keysyms.Up:
-                self.cursor_up()
-                return True
-            elif keyval == keysyms.Down:
-                self.cursor_down()
-                return True
-            elif keyval == keysyms.Left or keyval == keysyms.Right:
-                return True
-        if keyval in xrange(keysyms.a, keysyms.z + 1) or \
-            keyval in xrange(keysyms.A, keysyms.Z + 1):
-            if state & (modifier.CONTROL_MASK | modifier.ALT_MASK) == 0:
-                self.__preedit_string += unichr(keyval)
-                self.__invalidate()
-                return True
-        else:
-            if keyval < 128 and self.__preedit_string:
-                self.__commit_string(self.__preedit_string)
-
-        return False
-
-    def __invalidate(self):
-        if self.__is_invalidate:
-            return
-        self.__is_invalidate = True
-        gobject.idle_add(self.__update, priority = gobject.PRIORITY_LOW)
+			#Delete Last word
+			elif (ordered_pressed_keys == "gh"):
+				surrounding_text = self.get_surrounding_text()
+				text = surrounding_text[0].get_text().decode('UTF-8')
+				cursor_pos = surrounding_text[1]
+				string_up_to_cursor = text[:cursor_pos];
+				
+				#If end is space, then count backword till a space found  			
+				if (string_up_to_cursor[-1] == " "):
+					count = 0
+					char_found = 0;
+					
+					for item in string_up_to_cursor[::-1]:
+						if (item != " "):
+							char_found = 1;
+						if (item == " " and char_found == 1):
+							break;
+						count += 1
+					self.delete_surrounding_text(-(count),count);
+				
+				#If end is not space, delete length of last word	
+				else:
+					count = len(string_up_to_cursor.split()[-1])
+					self.delete_surrounding_text(-(count),count);	
 
 
-    def page_up(self):
-        if self.__lookup_table.page_up():
-            self.page_up_lookup_table()
-            return True
-        return False
+			#Delete Last letter
+			elif (ordered_pressed_keys == "h"):
+				self.delete_surrounding_text(-1,1);	
 
-    def page_down(self):
-        if self.__lookup_table.page_down():
-            self.page_down_lookup_table()
-            return True
-        return False
+			#Toggle capital switch
+			elif (ordered_pressed_keys == "g" and self.language == "english"):
+				self.capital_switch = 1;					
+				
+			else:
+				value = self.map[ordered_pressed_keys][self.braille_letter_map_pos]
+				if (self.capital_switch == 1):
+					value = value.upper()
+					self.capital_switch = 0;
+				self.__commit_string(value);
+				self.braille_letter_map_pos = 1;
+			return False
 
-    def cursor_up(self):
-        if self.__lookup_table.cursor_up():
-            self.cursor_up_lookup_table()
-            return True
-        return False
 
-    def cursor_down(self):
-        if self.__lookup_table.cursor_down():
-            self.cursor_down_lookup_table()
-            return True
-        return False
+		#Key press
+		else:
+			#self.__commit_string(keycode)
+			self.get_surrounding_text()
+			if keycode in self.keycode_map.keys():
+				#Store the letters
+				self.pressed_keys  += self.keycode_map[keycode];
+				return True
+			else:
+				if keyval == keysyms.space:
+					self.braille_letter_map_pos = 0;
+				else:
+					if (keycode == 119):
+						if self.language == "malayalam":
+							self.load_map("english en")
+						else:
+							self.load_map("malayalam ml")	
+				return False
+		
+    def load_map(self,language_with_code):
+		self.language = language_with_code.split()[0]
+		print ("loading Map for language : %s" %self.language)
+		self.map = {}
+		submap_number = 1;
+		self.append_sub_map("beginning.txt",submap_number);
+		submap_number = 2;
+		self.append_sub_map("middle.txt",submap_number);
+		submap_number = 3;
+		self.append_sub_map("punctuations.txt",submap_number);
+		
+		#Contraction dict 
+		self.contractions_dict = {};
+		
+		#load each contractions to map
+		for text_file in os.listdir("%s/braille/%s/"%(data_dir,self.language)):
+			if text_file not in ["beginning.txt","middle.txt","abbreviations.txt","abbreviations_default.txt","punctuations.txt","help.txt"]:
+				if "~" not in text_file:
+					submap_number += 1;
+					self.append_sub_map(text_file,submap_number);
+					self.contractions_dict[text_file[:-4]] = submap_number-1;
+		  
+		#Load abbreviations if exist
+		self.load_abbrivation();
+		
+
+
+	
+    def append_sub_map(self,filename,submap_number):
+		print("Loading sub map file for : %s with sn : %d " % (filename,submap_number))	
+		for line in open("%s/braille/%s/%s"%(data_dir,self.language,filename),"r"):
+			if (line.split(" ")[0]) in self.map.keys():
+				self.map[line.split(" ")[0]].append(line.split(" ")[1][:-1])
+				if len(self.map[line.split(" ")[0]]) != submap_number:
+					print("Repeated on : ",line.split(" ")[0])
+			else:
+				list=[];
+				for i in range (1,submap_number):
+					list.append(" ");
+				list.append(line.split(" ")[1][:-1]);
+				self.map[line.split(" ")[0]] = list;
+		
+		for key in self.map.keys():
+			if len(self.map[key]) < submap_number:
+				self.map[key].append(" ");
+
+
+    def load_abbrivation(self):
+		self.abbreviations = {}
+		try:
+			for line in open("%s/braille/%s/abbreviations.txt"%(data_dir,self.language),mode='r'):
+				self.abbreviations[line.split("  ")[0]] = line.split("  ")[1][:-1]
+		except FileNotFoundError:
+			pass
+
+
+
+    def order_pressed_keys(self,pressed_keys):
+		ordered = ""
+		for key in ["g","f","d","s","h","j","k","l","a",";"]:
+			if key in pressed_keys:
+				ordered += key;
+		return ordered;    
 
     def __commit_string(self, text):
         self.commit_text(ibus.Text(text))
-        self.__preedit_string = u""
-        self.__update()
-
-    def __update(self):
-        preedit_len = len(self.__preedit_string)
-        attrs = ibus.AttrList()
-        self.__lookup_table.clean()
-        if preedit_len > 0:
-            if not self.__dict.check(self.__preedit_string):
-                attrs.append(ibus.AttributeForeground(0xff0000, 0, preedit_len))
-                for text in self.__dict.suggest(self.__preedit_string):
-                    self.__lookup_table.append_candidate(ibus.Text(text))
-        self.update_auxiliary_text(ibus.Text(self.__preedit_string, attrs), preedit_len > 0)
-        attrs.append(ibus.AttributeUnderline(pango.UNDERLINE_SINGLE, 0, preedit_len))
-        self.update_preedit_text(ibus.Text(self.__preedit_string, attrs), preedit_len, preedit_len > 0)
-        self.__update_lookup_table()
-        self.__is_invalidate = False
-
-    def __update_lookup_table(self):
-        visible = self.__lookup_table.get_number_of_candidates() > 0
-        self.update_lookup_table(self.__lookup_table, visible)
-
-
-    def focus_in(self):
-        self.register_properties(self.__prop_list)
-
-    def focus_out(self):
-        pass
-
-    def reset(self):
-        pass
-
-    def property_activate(self, prop_name):
-        print "PropertyActivate(%s)" % prop_name
-
